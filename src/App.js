427a70802ab067a11a44db818d501d237b988ff4
Victoria Law <victoria.law@nbcuni.com>
2021-05-13 10:19:35 -0700

    Getting top rated movie data from TMDB API

--- src/App.js
+++ src/App.js
@@ -1,59 +1,116 @@
 import React, { Component } from "react";
 import { withStyles } from "@material-ui/styles";
 import Filter from "./components/filter.component";
+import MovieDetails from "./components/movie-details.component";
 import "./App.css";
+import axios from "axios";
 
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import { Typography } from "@material-ui/core";
 
 const styles = {
-  root: {
-    paddingTop: "15px",
-    align: "center",
-    justifyContent:"center", 
-    alignItems:"center",
-    textAlign: "center"
-  },
-  gridContainer: {
-    align: "center",
-    justifyContent:"center", 
-    alignItems:"center",
-    textAlign: "center",
-  }
+    wrapper: {
+        padding: "15px"
+    },
+    filter: {
+        align: "center",
+        justifyContent:"center", 
+        alignItems:"center",
+        textAlign: "center"
+    }
 };
 
 class App extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          filterStartDate: null,
-          filterEndDate: null
-      };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterStartDate: null,
+            filterEndDate: null,
+            page: 1,
+            listOfMovieIds: [],
+            maxNumOfPages: 0
+        };
+    }
 
-  handleFilterDateChange = (dateType, date) => {
-    this.setState({ [dateType]: date });
-  }
+    componentDidMount = () => {
+        this.getTopRatedMovies();
+    }
 
-  render = () => {
-    const { classes } = this.props;
+    getTopRatedMovies = () => {
+        const getRequestURL = "https://api.themoviedb.org/3/movie/top_rated?api_key=" + process.env.REACT_APP_API_KEY + "&language=en-US&page=" + this.state.page;
+            axios.get(getRequestURL)
+                .then(response => this.getListOfMovieIds(response.data));
+    }
 
-    return(
-      <div className={classes.root}>
-        <Grid container spacing={3} className={classes.gridContainer}>
-          <Grid item xs={3}>
-            <Filter 
-              filterStartDate = {this.state.filterStartDate}
-              filterEndDate = {this.state.filterEndDate}
-              handleFilterDateChange = {this.handleFilterDateChange}
-            />
-          </Grid>
-          <Grid item xs={8}>
-          </Grid>
-        </Grid>
-      </div>
-    );
-  }
+    getListOfMovieIds = (data) => {
+        const filteredListOfMovies = this.getFilteredListOfMovies(data.results);
+        const listOfMovieIds = this.state.listOfMovieIds.concat(filteredListOfMovies.map(movie => movie.id));
+        this.setState({ 
+            listOfMovieIds: listOfMovieIds,
+            maxNumOfPages: data.total_pages
+        });
+    }
+
+    getFilteredListOfMovies = (results) => {
+        console.log(results);
+        let filteredListOfMovies = results;
+        if (this.state.filterStartDate) {
+            filteredListOfMovies = filteredListOfMovies.filter((movie) => {
+                return new Date(movie.release_date) > new Date(this.state.filterStartDate)
+            });
+        }
+        if (this.state.filterEndDate) {
+            filteredListOfMovies = filteredListOfMovies.filter((movie) => {
+                return new Date(movie.release_date) < new Date(this.state.filterEndDate)
+            });
+        }
+        return filteredListOfMovies;
+    }
+
+    handleFilterDateChange = (dateType, date) => {
+        this.setState({ 
+            [dateType]: date,
+            listOfMovieIds: [],
+            page: 1
+        });
+        this.getTopRatedMovies();
+    }
+
+    render = () => {
+        const { classes } = this.props;
+
+        return(
+            <Grid container className={classes.wrapper}>
+                <Grid item xs={3} className={classes.filter}>
+                    <Filter
+                        filterStartDate = {this.state.filterStartDate}
+                        filterEndDate = {this.state.filterEndDate}
+                        handleFilterDateChange = {this.handleFilterDateChange}
+                    />
+                </Grid>
+                <Grid item xs={9}>
+                    {
+                        this.state.listOfMovieIds && this.state.listOfMovieIds.length > 0 ? 
+                            <List>
+                                {
+                                    this.state.listOfMovieIds.map((movieId, index) => {
+                                        return (
+                                            <ListItem key={index}>
+                                                <MovieDetails movieID = {movieId} />
+                                            </ListItem>
+                                        )
+                                    })
+                                }
+                            </List>
+                            :
+                            <Typography variant="h4" color="error">There are no search results found, please try again.</Typography>
+                    }
+                </Grid>
+            </Grid>
+        );
+    }
 }
 
 export default withStyles(styles)(App);
