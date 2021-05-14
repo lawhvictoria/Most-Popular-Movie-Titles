7ffeebd207d84b4a15e88c1fbcedc870c595f6ac
Victoria Law <victoria.law@nbcuni.com>
2021-05-13 21:22:11 -0700

    Replacing adult property with certification proerty to accurately reflect moving rating

--- src/components/movie-details.component.js
+++ src/components/movie-details.component.js
@@ -25,11 +25,11 @@ class MovieDetails extends Component {
             posterPath: null,
             title: null,
             releaseDate: null,
-            adult: false,
             runtime: null,
             genres: null,
             voteAverage: null,
-            metascore: null
+            metascore: null,
+            certification: null
         };
     }
 
@@ -44,19 +44,36 @@ class MovieDetails extends Component {
         this.setMovieProperties(response.data);
     }
 
+    getCertificationRating = (movieId) => {
+        const certificationURL = "https://api.themoviedb.org/3/movie/" + movieId + "/release_dates?api_key=" + process.env.REACT_APP_API_KEY;
+        axios.get(certificationURL)
+            .then(response => this.proccessCertificationResponse(response.data.results));
+    }
+
+    proccessCertificationResponse = (results) => {
+        let certificationResult = "";
+        const unitedStatesCertification = results.find((countryOfRelease) => {
+            return countryOfRelease.iso_3166_1 === "US"
+        });
+        if (unitedStatesCertification) {
+            certificationResult = unitedStatesCertification.release_dates[0].certification;
+        }
+        const certification = certificationResult === "" ? "Not Rated" : certificationResult;
+        this.setState({ certification: certification });
+    }
+
     getPosterImage = (posterPath) => {
         const posterURL = "https://image.tmdb.org/t/p/w500" + posterPath;
         this.setState({ posterPath: posterURL });
     }
 
     setMovieProperties = (data) => {
+        this.getCertificationRating(data.id);
         const convertedRuntime = this.convertMinsToHrsMins(data.runtime);
-        const movieRating = this.determineMovieRating(data.adult);
         const listOfGenres = this.getGenreNames(data.genres).join(", ");
         this.setState({
             title: data.original_title,
             releaseDate: data.release_date,
-            adult: movieRating,
             runtime: convertedRuntime,
             voteAverage: data.vote_average,
             metascore: data.popularity,
@@ -69,10 +86,6 @@ class MovieDetails extends Component {
         var mins = minutes % 60;
         return hours + " hour(s) " + mins + " minutes";
       }
-
-    determineMovieRating = (adult) => {
-        return adult ? "PG - 13" : "G";
-    }
     
     getGenreNames = (listOfGenres) => {
         return listOfGenres.map(genre => genre.name);
@@ -94,7 +107,7 @@ class MovieDetails extends Component {
                         <strong>{this.state.title}</strong> ({this.state.releaseDate})
                     </Typography>
                     <Typography variant="h6">
-                        Rated {this.state.adult} | {this.state.runtime} | {this.state.genres}
+                        {this.state.certification} | {this.state.runtime} | {this.state.genres}
                     </Typography>
                     <Typography variant="h6">
                         <strong>IMDB Rating:</strong> {this.state.voteAverage}/10 | <strong> Metascore:</strong> {this.state.metascore}
